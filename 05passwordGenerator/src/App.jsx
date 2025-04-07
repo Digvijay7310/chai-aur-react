5dab2be8477000b8fe9e47b30e125824116c9f35
Digvijay7310 <kumardigvijay377@gmail.com>
2025-04-07 14:22:06 +0530

    Doing Correction

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,9 +1,7 @@
 import { useState, useCallback, useEffect, useRef } from "react";
 import "./App.css";
-import Hello from "./Hello";
 
 function App() {
-  <Hello />;
   const [length, setLength] = useState(8);
   const [numberAllowed, setnumberAllowed] = useState(false);
   const [charAllowed, setcharAllowed] = useState(false);
